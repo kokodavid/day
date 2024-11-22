3699adff6508207a63b42b442e4d39f561034cfe
kokodavid <kingkgdave@gmail.com>
2024-11-22 20:41:57 +0300

    videos preloading and visual smoothening

--- lib/player/video_controller.dart
+++ lib/player/video_controller.dart
@@ -1,43 +1,77 @@
-import 'package:chewie/chewie.dart';
+import 'package:day/player/video_list_notifier.dart';
 import 'package:day/player/video_model.dart';
 import 'package:day/player/video_repository.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:video_player/video_player.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
+
+final currentVideoIndexProvider = StateProvider<int>((ref) => 0);
+
+final preloadedControllerProvider =
+    StateProvider<Map<String, YoutubePlayerController>>((ref) => {});
 
 final randomVideoProvider =
     FutureProvider.autoDispose<List<VideoModel>>((ref) async {
   return ref.read(videoRepositoryProvider).fetchVideo();
 });
 
-final videoControllerProvider =
-    Provider.family.autoDispose<YoutubePlayerController, String>((ref, data) {
-  final controller = YoutubePlayerController(
-      initialVideoId: data, flags: const YoutubePlayerFlags(autoPlay: true));
+final videoControllerProvider = Provider.family
+    .autoDispose<YoutubePlayerController, String>((ref, videoId) {
+  final preloadedControllers = ref.read(preloadedControllerProvider);
 
-  // ref.onDispose(() {
-  //   controller.dispose();
-  // });
+  if (preloadedControllers.containsKey(videoId)) {
+    final controller = preloadedControllers[videoId]!;
 
-  return controller;
+    Future.microtask(() {
+      ref.read(preloadedControllerProvider.notifier).state = {
+        ...preloadedControllers,
+      }..remove(videoId);
+    });
+
+    return controller;
+  }
+
+  return YoutubePlayerController(
+      initialVideoId: videoId,
+      flags: const YoutubePlayerFlags(autoPlay: true, hideThumbnail: true));
+  
 });
 
-final preloadNextVideoprovider = Provider.family<void,Map<String,dynamic>>((ref,data){
-  final currentIndex = data['currentIndex'];
-  final videos = data['videosList'];
+final preloadNextVideoProvider = Provider.family<void, int>((ref, currentIndex) {
+  final videos = ref.watch(videoListProvider);
+  if (currentIndex + 1 >= videos.length - 1) {
+    ref.read(videoListProvider.notifier).loadMoreVideos();
+  }
 
-  if(currentIndex + 1 < videos.length){
-    final nextController = ref.read(videoControllerProvider(videos[currentIndex + 1].id));
-    nextController.cue(videos[currentIndex + 1].id);
+  if (currentIndex == 3) {
+    ref.read(videoListProvider.notifier).trimList(currentIndex);
+  }
+
+  if (currentIndex + 1 < videos.length) {
+    final nextVideo = videos[currentIndex + 1];
+    final preloadedControllers = ref.read(preloadedControllerProvider);
+
+    if (!preloadedControllers.containsKey(nextVideo.id)) {
+      Future.microtask(() {
+        final nextController = ref.read(videoControllerProvider(nextVideo.id));
+
+        nextController
+          ..cue(nextVideo.id)
+          ..pause();
+
+        ref.read(preloadedControllerProvider.notifier).state = {
+          ...preloadedControllers,
+          nextVideo.id: nextController,
+        };
+      });
+    }
   }
 });
 
-final chewieControllerProvider =
-    Provider.family<ChewieController, String>((ref, id) {
-  final videoPlayerController = VideoPlayerController.networkUrl(
-      Uri.parse('http://www.youtube.com/watch?v=$id'));
-  return ChewieController(
-      videoPlayerController: videoPlayerController,
-      autoPlay: true,
-      looping: true);
+final cleanupControllersProvider = Provider<void>((ref) {
+  ref.onDispose(() {
+    final preloadedControllers = ref.read(preloadedControllerProvider);
+    for (final controller in preloadedControllers.values) {
+      controller.dispose();
+    }
+  });
 });
--- lib/player/video_player.dart
+++ lib/player/video_player.dart
@@ -1,6 +1,7 @@
 import 'dart:developer';
 
 import 'package:day/player/video_controller.dart';
+import 'package:day/player/video_list_notifier.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:youtube_player_flutter/youtube_player_flutter.dart';
@@ -27,50 +28,48 @@ class _VideoPlayerState extends ConsumerState<VideoPlayer> {
     super.dispose();
   }
 
-  @override
-  Widget build(BuildContext context) {
-    final videoData = ref.watch(randomVideoProvider);
-    return Scaffold(
-        body: videoData.when(
-            data: (videos) => PageView.builder(
-                scrollDirection: Axis.vertical,
-                controller: pageController,
-                onPageChanged: (index) {
-                  final dataMap = {'videosList': videos, 'currentIndex': index};
+@override
+Widget build(BuildContext context) {
+  final videoList = ref.watch(videoListProvider); 
 
-                  if (index + 1 < videos.length) {
-                    return ref.read(preloadNextVideoprovider(dataMap));
-                  }
-                },
-                itemBuilder: (context, index) {
-                  if (index >= videos.length) {
-                    return const Center(
-                      child: Text('No more videos'),
-                    );
-                  }
-                  final video = videos[index];
-                  final controller =
-                      ref.read(videoControllerProvider(video.id));
-
-                      if (index == 0){
-                        WidgetsBinding.instance.addPostFrameCallback((_){
-                          ref.read(preloadNextVideoprovider({'videosList':videos,'currentIndex':index}));
-                        });
-                      }
-                  return YoutubePlayer(
-                      controller: controller,
-                      showVideoProgressIndicator: true,
-                      progressIndicatorColor: Colors.red,
-                      aspectRatio: 9 / 16,
-                      onEnded: (metadata) {
-                        pageController.nextPage(
-                            duration: const Duration(microseconds: 300),
-                            curve: Curves.easeInOut);
-                      });
-                }),
-            error: (e, s) {
-              return Text('Error Occured $e,$s');
-            },
-            loading: () => const Center(child: CircularProgressIndicator())));
+  if (videoList.isEmpty) {
+    ref.read(videoListProvider.notifier).initializeVideos();
+    return const Center(child: CircularProgressIndicator());
   }
+
+  return Scaffold(
+    body: PageView.builder(
+      scrollDirection: Axis.vertical,
+      controller: pageController,
+      onPageChanged: (index) {
+        ref.read(currentVideoIndexProvider.notifier).state = index;
+
+        ref.read(preloadNextVideoProvider(index));
+      },
+      itemBuilder: (context, index) {
+        if (index >= videoList.length) {
+          return const Center(
+            child: Text('No more videos'),
+          );
+        }
+
+        final video = videoList[index];
+        final controller = ref.read(videoControllerProvider(video.id));
+
+        return YoutubePlayer(
+          controller: controller,
+          showVideoProgressIndicator: true,
+          progressIndicatorColor: Colors.red,
+          aspectRatio: 9 / 16,
+          onReady: () {
+            controller.play();
+          },
+          onEnded: (metadata) {
+            controller.pause();
+          },
+        );
+      },
+    ),
+  );
+}
 }
